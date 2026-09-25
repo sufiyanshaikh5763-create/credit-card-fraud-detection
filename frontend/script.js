52113e1f1d23d2ae21a86ce60e6a62cf699422d3
Sufiyan shaikh <sufiyanshaikh5763@gmail.com>
2026-09-25 19:33:24 +0530

    Connect frontend to deployed API

--- frontend/script.js
+++ frontend/script.js
@@ -202,7 +202,7 @@ form.addEventListener(
             ----------------------------------------- */
 
             const response = await fetch(
-                "http://127.0.0.1:8000/predict",
+                "http://credit-card-fraud-detection-bwbf.onrender.com/predict",
                 {
                     method: "POST",
 
